8598d4f2c43ce0557f788138562ba50bea2c791b
Rene Wang <rene.wang1@hotmail.com>
2020-12-01 19:35:49 +0000

    timer function working

--- timer1.js
+++ timer1.js
@@ -1,1 +1,13 @@
-process.stdout.write('\x07');
+const args = process.argv.slice(2);
+const timer = () => {
+  args.sort(function(a, b) {
+    return a - b;
+  });
+  for (let item of args) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, item * 1000);
+  }
+};
+
+timer();
