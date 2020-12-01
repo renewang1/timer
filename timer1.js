aa0176cb4bc312bde57a8f9a3243765739937eb1
Rene Wang <rene.wang1@hotmail.com>
2020-12-01 19:38:52 +0000

    timer function passes edge cases

--- timer1.js
+++ timer1.js
@@ -1,9 +1,9 @@
 const args = process.argv.slice(2);
 const timer = () => {
-  args.sort(function(a, b) {
-    return a - b;
-  });
-  for (let item of args) {
+  const times = args.sort(function(a, b) {
+      return a - b;
+    }).filter(num => num > 0);
+  for (let item of times) {
     setTimeout(() => {
       process.stdout.write('\x07');
     }, item * 1000);
